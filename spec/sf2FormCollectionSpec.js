8fde114c6825b6b96f63c4794936daa83cd7f833
Bruno Paulin <brunopaulin@bpaulin.net>
2013-06-09 17:55:07 +0200

    add class to items

--- spec/sf2FormCollectionSpec.js
+++ spec/sf2FormCollectionSpec.js
@@ -1,14 +1,25 @@
 describe("sf2FormCollection", function() {
   beforeEach(function () {
     $('<div id="container"></div>').appendTo('body');
+    for (var i=0; i < 5; i++) {
+      $('<div></div>').appendTo('#container');
+    };
   });
 
   afterEach(function () {
-      $("#container").remove();
+    $("#container").remove();
   });
 
   it('should be testable', function () {
-    var div = $("container");
-    expect(div.sf2FormCollection).toBeDefined();
+    var container = $("#container");
+    expect(container.sf2FormCollection).toBeDefined();
+  });
+
+  it('should add a class to each item', function () {
+    var container = $("#container");
+    container.sf2FormCollection();
+    container.children('*').each(function () {
+      expect($(this).hasClass('sf2fc-item')).toBeTruthy();
+    });
   });
 });
--- src/sf2FormCollection.js
+++ src/sf2FormCollection.js
@@ -1,5 +1,10 @@
 (function($) {
     $.fn.sf2FormCollection=function(options)
-    {
+    {        
+    	$(this).children('*').each(function () {
+		  $(this).addClass('sf2fc-item');
+		});
+
+    	return $(this);
     };
 })(jQuery);
