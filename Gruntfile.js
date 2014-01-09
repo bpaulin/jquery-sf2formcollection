b9a67f80fc2fb8c018873dfdfc257a45a8a388e2
Bruno Paulin <brunopaulin@bpaulin.net>
2014-01-09 22:30:47 +0100

    build status in readme

--- Gruntfile.js
+++ Gruntfile.js
@@ -47,6 +47,7 @@ module.exports = function(grunt) {
 			}
 		},
 
+		// Jasmine definitions
 	    jasmine : {
 			src : 'src/**/*.js',
 			options : {
@@ -60,6 +61,7 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks("grunt-contrib-jshint");
 	grunt.loadNpmTasks("grunt-contrib-uglify");
 	grunt.loadNpmTasks('grunt-contrib-jasmine');
+	grunt.loadNpmTasks('grunt-contrib-qunit');
 
 	grunt.registerTask("default", ["jshint", "jasmine", "concat", "uglify"]);
 	grunt.registerTask("travis", ["jshint", "jasmine"]);
