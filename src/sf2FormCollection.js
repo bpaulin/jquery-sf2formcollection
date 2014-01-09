0e50d86497f86d145b3cc4f5aa89d4656662e128
Bruno Paulin <brunopaulin@bpaulin.net>
2014-01-10 00:09:24 +0100

    cleaning js

--- src/sf2FormCollection.js
+++ src/sf2FormCollection.js
@@ -23,108 +23,101 @@
 
         // The actual plugin constructor
         function Plugin ( element, options ) {
-                this.element = element;
-                // jQuery has an extend method which merges the contents of two or
-                // more objects, storing the result in the first object. The first object
-                // is generally empty as we don't want to alter the default options for
-                // future instances of the plugin
-                this.settings = $.extend( {}, defaults, options );
-                this._defaults = defaults;
-                this._name = pluginName;
-                this.init();
-                this.sf2FormCollection();
+            this.element = element;
+            // jQuery has an extend method which merges the contents of two or
+            // more objects, storing the result in the first object. The first object
+            // is generally empty as we don't want to alter the default options for
+            // future instances of the plugin
+            this.settings = $.extend( {}, defaults, options );
+            this._defaults = defaults;
+            this._name = pluginName;
+            this.init();
         }
 
         Plugin.prototype = {
-                init: function () {
-                        // Place initialization logic here
-                        // You already have access to the DOM element and
-                        // the options via the instance, e.g. this.element
-                        // and this.settings
-                        // you can add more functions like the one below and
-                        // call them like so: this.yourOtherFunction(this.element, this.settings).
-                        console.log("sf2FormCollection");
-                },
+            init: function () {
+                // Place initialization logic here
+                // You already have access to the DOM element and
+                // the options via the instance, e.g. this.element
+                // and this.settings
+                // you can add more functions like the one below and
+                // call them like so: this.yourOtherFunction(this.element, this.settings).
+                var that = this,
+                    containerAddElement = $("<div class=\"sf2fc-add\"></div>"),
+                    addElement = $(this.settings.addItem),
+                    /** Move Original items */
+                    items = $("<div class=\"sf2fc-items\"></div>");
 
-                sf2FormCollection: function () {
-                        // some logic
-                        var that = this,
-                        container = $(this.element),
-                        containerAddElement = $("<div class=\"sf2fc-add\"></div>"),
-                        addElement = $(this.settings.addItem),
+                $(this.element).children("*").each( function() {
+                    item = $("<div class=\"sf2fc-item\"></div>");
+                    $(this).detach().appendTo(item);
+                    item.appendTo(items);
+                });
+                items.appendTo($(this.element));
+                $(this.element).data("index", items.contents().length);
 
-                        /** Move Original items */
-                        items = $("<div class=\"sf2fc-items\"></div>");
-                        container.children("*").each( function() {
-                            item = $("<div class=\"sf2fc-item\"></div>");
-                            $(this).detach().appendTo(item);
-                            item.appendTo(items);
+                /** AddElement */
+                addElement.attr("id","sf2fc-add");
+                addElement.appendTo(containerAddElement);
+                containerAddElement.appendTo($(this.element));
+
+                /** Click on AddElement */
+                containerAddElement.on("click", function(e) {
+                    e.preventDefault();
+                    var prototype = $(that.element).data("prototype"),
+                        index = $(that.element).data("index"),
+                        re = new RegExp(that.settings.tokenIndex, "g");
+                    prototype = prototype.replace(re, index);
+                    item = $("<div class=\"sf2fc-item\"></div>");
+                    item.append(prototype);
+
+                    link = $(that.settings.removeItem);
+                    link.addClass("sf2fc-remove");
+                    link.click(function() {
+                        $(this).parent().remove();
+                    });
+                    item.append(link);
+
+                    /** SortElement */
+                    if (that.settings.sortable) {
+                        link = $(that.settings.sortItem);
+                        link.addClass("sf2fc-sort");
+                        item.prepend(link);
+                    }
+
+                    $(that.element).find(".sf2fc-items").append(item);
+
+                    $(that.element).data("index", index+1);
+                });
+
+                /** RemoveElement */
+                if (this.settings.removeItem !== "") {
+                    $(this.element).find(".sf2fc-items").children("*").each(function () {
+                        link = $(that.settings.removeItem);
+                        link.addClass("sf2fc-remove");
+                        $(this).append(link);
+                        link.click(function() {
+                            $(this).parent().remove();
                         });
-                        items.appendTo(container);
-                        container.data("index", items.contents().length);
-
-                        /** AddElement */
-                        addElement.attr("id","sf2fc-add");
-                        addElement.appendTo(containerAddElement);
-                        containerAddElement.appendTo($(this.element));
-
-                        /** Click on AddElement */
-                        containerAddElement.on("click", function(e) {
-                            e.preventDefault();
-                            var prototype = container.data("prototype"),
-                                index = container.data("index"),
-                                re = new RegExp(that.settings.tokenIndex, "g");
-                            prototype = prototype.replace(re, index);
-                            item = $("<div class=\"sf2fc-item\"></div>");
-                            item.append(prototype);
-
-                            link = $(that.settings.removeItem);
-                            link.addClass("sf2fc-remove");
-                            link.click(function() {
-                                $(this).parent().remove();
-                            });
-                            item.append(link);
-
-                            /** SortElement */
-                            if (that.settings.sortable) {
-                                link = $(that.settings.sortItem);
-                                link.addClass("sf2fc-sort");
-                                item.prepend(link);
-                            }
-
-                            container.find(".sf2fc-items").append(item);
-
-                            container.data("index", index+1);
-                        });
-
-                        /** RemoveElement */
-                        if (this.settings.removeItem !== "") {
-                            container.find(".sf2fc-items").children("*").each(function () {
-                                link = $(that.settings.removeItem);
-                                link.addClass("sf2fc-remove");
-                                $(this).append(link);
-                                link.click(function() {
-                                    $(this).parent().remove();
-                                });
-                            });
-                        }
-
-                        /** SortElement */
-                        if (this.settings.sortable) {
-                            container.find(".sf2fc-items").sortable({
-                                cursor: "move",
-                                handle: ".sf2fc-sort"
-                            });
-                            container.find(".sf2fc-items").children("*").each(function () {
-                                link = $(that.settings.sortItem);
-                                link.addClass("sf2fc-sort");
-                                $(this).prepend(link);
-                            });
-                        }
-
-                        /** return */
-                        return $(this);
+                    });
                 }
+
+                /** SortElement */
+                if (this.settings.sortable) {
+                    $(this.element).find(".sf2fc-items").sortable({
+                        cursor: "move",
+                        handle: ".sf2fc-sort"
+                    });
+                    $(this.element).find(".sf2fc-items").children("*").each(function () {
+                        link = $(that.settings.sortItem);
+                        link.addClass("sf2fc-sort");
+                        $(this).prepend(link);
+                    });
+                }
+
+                /** return */
+                return $(this);
+            }
         };
 
         // A really lightweight plugin wrapper around the constructor,
