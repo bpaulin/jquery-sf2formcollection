5b49811c212f8098c4f09637209e6ef11af200da
Bruno Paulin <brunopaulin@bpaulin.net>
2014-01-09 18:45:55 +0100

    boilerplate structure

---
+++ Gruntfile.js
@@ -0,0 +1,69 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+
+		// Import package manifest
+		pkg: grunt.file.readJSON("sf2FormCollection.jquery.json"),
+
+		// Banner definitions
+		meta: {
+			banner: "/*\n" +
+				" *  <%= pkg.title || pkg.name %> - v<%= pkg.version %>\n" +
+				" *  <%= pkg.description %>\n" +
+				" *  <%= pkg.homepage %>\n" +
+				" *\n" +
+				" *  Made by <%= pkg.author.name %>\n" +
+				" *  Under <%= pkg.licenses[0].type %> License\n" +
+				" */\n"
+		},
+
+		// Concat definitions
+		concat: {
+			dist: {
+				src: ["src/sf2FormCollection.js"],
+				dest: "dist/sf2FormCollection.js"
+			},
+			options: {
+				banner: "<%= meta.banner %>"
+			}
+		},
+
+		// Lint definitions
+		jshint: {
+			files: ["src/sf2FormCollection.js"],
+			options: {
+				jshintrc: ".jshintrc"
+			}
+		},
+
+		// Minify definitions
+		uglify: {
+			my_target: {
+				src: ["dist/sf2FormCollection.js"],
+				dest: "dist/sf2FormCollection.min.js"
+			},
+			options: {
+				banner: "<%= meta.banner %>"
+			}
+		},
+
+		// CoffeeScript compilation
+		coffee: {
+			compile: {
+				files: {
+					"dist/sf2FormCollection.js": "src/jquery.sf2FormCollection.coffee"
+				}
+			}
+		}
+
+	});
+
+	grunt.loadNpmTasks("grunt-contrib-concat");
+	grunt.loadNpmTasks("grunt-contrib-jshint");
+	grunt.loadNpmTasks("grunt-contrib-uglify");
+	grunt.loadNpmTasks("grunt-contrib-coffee");
+
+	grunt.registerTask("default", ["jshint", "concat", "uglify"]);
+	grunt.registerTask("travis", ["jshint"]);
+
+};
