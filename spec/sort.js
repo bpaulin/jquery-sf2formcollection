c1b68c0b449a2ff1cdfd046ed6e80a84d08f99d1
Bruno Paulin <brunopaulin@bpaulin.net>
2014-01-19 01:53:36 +0100

    add selectorIndex parameter
     * node matching "selectorIndex" inside an item will contain new index after sort

--- spec/sort.js
+++ spec/sort.js
@@ -55,4 +55,79 @@ describe("sf2FormCollection can sort items", function() {
       expect($(this).children().first().hasClass('sf2fc-sort')).toBeTruthy();
     });
   });
+
+  it('should not update $(selectorIndex) if not supplied', function () {
+    var container = $("#collection");
+    var settings = {
+      'sortItem': 'test <a href="#">Sort this item</a>'
+    };
+    container.sf2FormCollection(settings);
+    $('.sf2fc-add').click();
+
+    container.find('.sf2fc-items').find('.sf2fc-item').each(
+      function (index) {
+        expect(
+          $(this).find('span.index').html()
+        ).not.toEqual(index.toString());
+      }
+    );
+  });
+
+  it('should update $(selectorIndex) if supplied after init and add', function () {
+    var container = $("#collection");
+    var settings = {
+      'sortItem': 'test <a href="#">Sort this item</a>',
+      "selectorIndex": "span.index"
+    };
+    container.sf2FormCollection(settings);
+    $('.sf2fc-add').click();
+
+    container.find('.sf2fc-items').find('.sf2fc-item').each(
+      function (index) {
+        expect(
+          $(this).find('span.index').html()
+        ).toEqual(index.toString());
+      }
+    );
+  });
+
+  it('should update $(selectorIndex) if supplied after sort', function () {
+    var container = $("#collection");
+    var settings = {
+      'sortItem': 'test <a href="#">Sort this item</a>',
+      "selectorIndex": "span.index"
+    };
+    container.sf2FormCollection(settings);
+    $('.sf2fc-add').click();
+    container.find('.sf2fc-items').find('.sf2fc-item').first().appendTo(
+      container.find('.sf2fc-items')
+    );
+    container.find('.sf2fc-items').trigger('update');
+    container.find('.sf2fc-items').find('.sf2fc-item').each(
+      function (index) {
+        expect(
+          $(this).find('span.index').html()
+        ).toEqual(index.toString());
+      }
+    );
+  });
+
+  it('should update $(selectorIndex) if supplied after remove', function () {
+    var container = $("#collection");
+    var settings = {
+      'sortItem': 'test <a href="#">Sort this item</a>',
+      "selectorIndex": "span.index"
+    };
+    container.sf2FormCollection(settings);
+
+    container.find('.sf2fc-items .sf2fc-item:eq(2) .sf2fc-remove').click();
+    container.find('.sf2fc-items').trigger('update');
+    container.find('.sf2fc-items').find('.sf2fc-item').each(
+      function (index) {
+        expect(
+          $(this).find('span.index').html()
+        ).toEqual(index.toString());
+      }
+    );
+  });
 });
